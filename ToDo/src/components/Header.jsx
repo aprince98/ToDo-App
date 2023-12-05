54e254f215a049340d2b619d16fee801b44edc22
aprince98 <122805943+aprince98@users.noreply.github.com>
2023-12-05 12:00:58 +0000

    First commit

---
+++ ToDo/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import styles from "../style.module.css"
+
+const Header=()=>{
+    return (
+        <div className={styles.header}>
+            <h3 className={styles.headerh1}>ToDo App</h3>
+        </div>
+    )
+}
+
+export default Header
---
+++ ToDo/src/components/TodoList.jsx
@@ -0,0 +1,20 @@
+import styles from "../style.module.css"
+import Todo from "./Todo"
+
+const TodoList =({todoList, setTodoList}) => {
+    return(
+        <div>
+            {todoList.map((todoItem)=>(
+                <Todo 
+                setTodoList={setTodoList}
+                key={todoItem.id} 
+                todoItem={todoItem} 
+                todoList={todoList}>
+
+                </Todo>
+            ))}
+        </div>
+    )
+}
+
+export default TodoList 
